d22d513e7c43a2210ca3b8738866922960815d38
Andrey Dyldin <and@cesbo.com>
2018-10-29 00:10:24 +0500

    tune: dvb-s2

--- src/tune.rs
+++ src/tune.rs
@@ -3,7 +3,7 @@
 use libc;
 use frontend;
 
-use std::io;
+use std::{io, time, thread};
 use std::os::unix::io::RawFd;
 
 pub trait Dvb {
@@ -13,9 +13,9 @@ pub trait Dvb {
 /// Adapter
 pub struct Adapter {
     /// Adapter number /dev/dvb/adapterX
-    pub id: usize,
+    pub id: u32,
     /// Device number /dev/dvb/adapterX/frontendX
-    pub device: usize,
+    pub device: u32,
     /// Modulation
     pub modulation: u32,
 }
@@ -61,11 +61,11 @@ fn close(fd: RawFd) {
 /// DVB-S/S2 Unicable options
 pub struct Unicable10 {
     /// Slot range from 1 to 8
-    pub slot: usize,
+    pub slot: u32,
     /// Frequency range from 950 to 2150 MHz
-    pub frequency: usize,
+    pub frequency: u32,
     /// Position range from 1 to 2
-    pub position: usize,
+    pub position: u32,
 }
 
 /// DVB-S/S2 LNB mode
@@ -76,11 +76,11 @@ pub enum LnbMode {
     /// Send 22kHz tone to LNB
     TONE,
     /// Tone Burst port range from 1 to 2
-    TONEBURST(usize),
+    TONEBURST(u32),
     /// DiSEqC 1.0 port range from 1 to 4
-    DISEQC_1_0(usize),
+    DISEQC_1_0(u32),
     /// DiSEqC 1.1 port range from 1 to 16
-    DISEQC_1_1(usize),
+    DISEQC_1_1(u32),
     /// EN50494 / Unicable
     UNICABLE_1_0(Unicable10),
     /// Disable LNB
@@ -90,11 +90,11 @@ pub enum LnbMode {
 /// DVB-S/S2 Transponder
 pub struct Transponder {
     /// Frequency
-    pub frequency: usize,
+    pub frequency: u32,
     /// Polarization: SEC_VOLTAGE_13 for V/R, SEC_VOLTAGE_18 for H/L
     pub polarization: u32,
     /// Symbol-rate
-    pub symbolrate: usize,
+    pub symbolrate: u32,
 }
 
 /// DVB-S/S2 LNB
@@ -102,11 +102,11 @@ pub struct Lnb {
     /// Mode
     pub mode: LnbMode,
     /// Low band frequency
-    pub lof1: usize,
+    pub lof1: u32,
     /// High band frequency
-    pub lof2: usize,
+    pub lof2: u32,
     /// Threshold frequency - threshold between low and high band
-    pub slof: usize,
+    pub slof: u32,
 }
 
 /// DVB-S2 Options
@@ -116,21 +116,85 @@ pub struct DvbS2 {
     pub lnb: Lnb,
     pub fec: u32,
     pub rof: u32,
+    pub mis: u32,
 }
 
 impl Dvb for DvbS2 {
     fn open(&self) -> io::Result<RawFd> {
         let fd = open(&self.adapter)?;
+        clear(fd)?;
+
+        let mut frequency: u32 = self.transponder.frequency;
+        let mut tone = frontend::SEC_TONE_OFF;
+
+        if self.lnb.lof1 > 0 {
+            if self.lnb.slof > 0 &&
+                self.lnb.lof2 > 0 &&
+                frequency >= self.lnb.slof
+            {
+                /* hiband */
+                frequency -= self.lnb.lof2;
+                tone = frontend::SEC_TONE_ON;
+            }
+            else
+            {
+                if self.lnb.lof1 > frequency {
+                    frequency = self.lnb.lof1 - frequency;
+                } else {
+                    frequency -= self.lnb.lof1;
+                }
+            }
+        } else {
+            if frequency >= 950 && frequency <= 2150 {
+                //
+            } else if frequency >= 2500 && frequency <= 2700 {
+                frequency = 3650 - frequency;
+            } else if frequency >= 3400 && frequency <= 4200 {
+                frequency = 5150 - frequency;
+            } else if frequency >= 4500 && frequency <= 4800 {
+                frequency = 5950 - frequency;
+            } else if frequency >= 10700 && frequency < 11700 {
+                frequency -= 9750;
+            } else if frequency >= 11700 && frequency < 13250 {
+                frequency -= 10600;
+                tone = frontend::SEC_TONE_ON;
+            } else {
+                return Err(io::Error::from_raw_os_error(libc::EINVAL));
+            }
+        }
+        frequency *= 1000;
+
+        match self.lnb.mode {
+            LnbMode::AUTO => {
+                frontend::set_tone(fd, frontend::SEC_TONE_OFF)?;
+                frontend::set_voltage(fd, self.transponder.polarization)?;
+                thread::sleep(time::Duration::from_millis(100));
+                frontend::set_tone(fd, tone)?;
+                thread::sleep(time::Duration::from_millis(100));
+            },
+            LnbMode::TONE => {
+                frontend::set_tone(fd, frontend::SEC_TONE_OFF)?;
+                frontend::set_voltage(fd, self.transponder.polarization)?;
+                thread::sleep(time::Duration::from_millis(100));
+                frontend::set_tone(fd, frontend::SEC_TONE_ON)?;
+                thread::sleep(time::Duration::from_millis(100));
+            },
+            LnbMode::OFF => {
+                frontend::set_tone(fd, frontend::SEC_TONE_OFF)?;
+                frontend::set_voltage(fd, frontend::SEC_VOLTAGE_OFF)?;
+            },
+            _ => {},
+        };
 
         let cmdseq = vec![
             frontend::Property::new(frontend::DTV_DELIVERY_SYSTEM, frontend::SYS_DVBS2),
-            frontend::Property::new(frontend::DTV_FREQUENCY, 0), // TODO
+            frontend::Property::new(frontend::DTV_FREQUENCY, frequency),
             frontend::Property::new(frontend::DTV_MODULATION, self.adapter.modulation),
             frontend::Property::new(frontend::DTV_INVERSION, frontend::INVERSION_AUTO),
-            frontend::Property::new(frontend::DTV_SYMBOL_RATE, 0), // TODO
+            frontend::Property::new(frontend::DTV_SYMBOL_RATE, self.transponder.symbolrate * 1000),
             frontend::Property::new(frontend::DTV_INNER_FEC, self.fec),
             frontend::Property::new(frontend::DTV_PILOT, frontend::PILOT_AUTO),
-            frontend::Property::new(frontend::DTV_ROLLOFF, frontend::ROLLOFF_35),
+            frontend::Property::new(frontend::DTV_ROLLOFF, self.rof),
             frontend::Property::new(frontend::DTV_STREAM_ID, 0),
             frontend::Property::new(frontend::DTV_SCRAMBLING_SEQUENCE_INDEX, 0),
             frontend::Property::new(frontend::DTV_TUNE, 0),
